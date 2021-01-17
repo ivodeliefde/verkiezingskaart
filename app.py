0ea51aa9ad7c4368f98ec71035fdeebc8d2606bf
Ivo de Liefde <ivodeliefde@gmail.com>
2021-01-17 14:31:01 +0100

    Sidebar toegevoegd voor selecties.

--- app.py
+++ app.py
@@ -22,76 +22,62 @@ resultaat = {
 gemeenten = gpd.read_file(os.path.join(os.getcwd(), "data", "municipalities_simplified_2.geojson"))
 gemeenten = gemeenten.merge(uitslag, on="gemeentenaam")
 
-col1, col2 = st.beta_columns([4,3])
-col3, col4, col5 = st.beta_columns([1,3,3])
 
-st.write(uitslag)
+statistiek = st.sidebar.radio(
+    "",
+    ('Grootste Partij', 'Grootste Winnaar', 'Grootste Verliezer'))
 
-with col3:
-    statistiek = st.radio(
-        "",
-        ('Grootste Partij', 'Grootste Winnaar', 'Grootste Verliezer'))
+# map_style = st.radio(
+#     "",
+#     ('Gemeentegrenzen', 'Gemeenten als Cartogram'))
 
-    # map_style = st.radio(
-    #     "",
-    #     ('Gemeentegrenzen', 'Gemeenten als Cartogram'))
+gemeente = st.sidebar.selectbox(
+    'Gemeente',
+    (gemeenten.gemeentenaam.sort_values()))
 
-    gemeente = st.selectbox(
-        'Gemeente',
-        (gemeenten.gemeentenaam.sort_values()))
+partij = st.sidebar.selectbox(
+    'Partij',
+    (partijen))
 
-    partij = st.selectbox(
-        'Partij',
-        (partijen))
+st.title("Verkiezingen Tweede Kamer 2021")
 
-with col1:
-    st.title("Verkiezingen Tweede Kamer 2021")
-
-    st.write(alt.Chart(uitslag_totaal_stemmen, width=1000, height=485).mark_bar().encode(
-        x=alt.X('Partij', sort="-y"),
-        y='Aantal stemmen',
-        color=alt.condition(
-            alt.datum.Partij == resultaat[statistiek],
-            alt.value('red'),
-            alt.value('lightgrey')
-        )
-    ).configure_axisX(labelAngle=-45))
-
-with col2:
-    m = folium.Map(location=[52.35, 5.3878],
-                   zoom_start=7,
-                   tiles='https://api.mapbox.com/styles/v1/ivo11235/ckjx01y2e1brw17nqdictt5wk/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiaXZvMTEyMzUiLCJhIjoieV82bFVfNCJ9.G8mrfJOA07edDDj6Bep2bQ',
-                   attr='© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <a href="https://www.mapbox.com/map-feedback/">(Improve this map)</a>',
-                   height=485)
-
-
-    choropleth = folium.Choropleth(geo_data = gemeenten,
-                 data=uitslag,
-                 columns=['gemeentenaam', f"Percentage {resultaat[statistiek]}"],
-                 key_on='feature.properties.gemeentenaam',
-                 # fill_color='YlGnBu',
-                 bins = list(range(0, 51, 10)),
-                 # fill_opacity=0.4,
-                 line_opacity=0.2,
-                 legend_name=resultaat[statistiek],
-                 smooth_factor=1).add_to(m)
-
-    # choropleth = folium.GeoJson(
-    #         data = gemeenten,
-    #         fill_color='YlGnBu',
-    #         smooth_factor=1).add_to(m)
-
-    choropleth.geojson.add_child(
-        folium.features.GeoJsonTooltip(['gemeentenaam'])
+st.write(alt.Chart(uitslag_totaal_stemmen, width=1000, height=485).mark_bar().encode(
+    x=alt.X('Partij', sort="-y"),
+    y='Aantal stemmen',
+    color=alt.condition(
+        alt.datum.Partij == resultaat[statistiek],
+        alt.value('red'),
+        alt.value('lightgrey')
     )
+).configure_axisX(labelAngle=-45))
 
-    folium_static(m)
+m = folium.Map(location=[52.35, 5.3878],
+               zoom_start=7,
+               tiles='https://api.mapbox.com/styles/v1/ivo11235/ckjx01y2e1brw17nqdictt5wk/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiaXZvMTEyMzUiLCJhIjoieV82bFVfNCJ9.G8mrfJOA07edDDj6Bep2bQ',
+               attr='© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <a href="https://www.mapbox.com/map-feedback/">(Improve this map)</a>',
+               height=485)
 
 
-with col4:
-    st.write("x")
+choropleth = folium.Choropleth(geo_data = gemeenten,
+             data=uitslag,
+             columns=['gemeentenaam', f"Percentage {resultaat[statistiek]}"],
+             key_on='feature.properties.gemeentenaam',
+             # fill_color='YlGnBu',
+             bins = list(range(0, 51, 10)),
+             # fill_opacity=0.4,
+             line_opacity=0.2,
+             legend_name=resultaat[statistiek],
+             smooth_factor=1).add_to(m)
 
+# choropleth = folium.GeoJson(
+#         data = gemeenten,
+#         fill_color='YlGnBu',
+#         smooth_factor=1).add_to(m)
 
-with col5:
-    st.write("x")
+choropleth.geojson.add_child(
+    folium.features.GeoJsonTooltip(['gemeentenaam'])
+)
+
+folium_static(m)
+
 
